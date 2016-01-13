6eb735f00fca7bd0d4f5fc6c5ce7decbe0f9349e
Martin Lv <lvmenglineric@gmail.com>
2016-01-14 00:02:10 +0800

    add MLAsyncOperation & test code

---
+++ asyncOperation/MLAsyncOperation.swift
@@ -0,0 +1,76 @@
+//
+//  MLAsyncOperation.swift
+//  asyncOperation
+//
+//  Created by 吕孟霖 on 16/1/13.
+//  Copyright © 2016年 lvmenglin. All rights reserved.
+//
+
+import Cocoa
+
+typealias MLAsyncOperationBlock = (operation:MLAsyncOperation?)->Void
+
+class MLAsyncOperation: NSOperation {
+    private var ml_executing = false
+    private var ml_finished = false
+    
+    private var block:MLAsyncOperationBlock?
+    
+    override var asynchronous:Bool {
+        return true
+    }
+    
+    override var concurrent:Bool {
+        return true
+    }
+    
+    override var finished:Bool{
+        return ml_finished
+    }
+    
+    override var executing:Bool{
+        return ml_executing
+    }
+    
+    func ml_setFinished(finished:Bool){
+        willChangeValueForKey("isFinished")
+        ml_finished = finished
+        didChangeValueForKey("isFinished")
+    }
+    
+    func ml_setExecuting(executing:Bool){
+        willChangeValueForKey("isExecuting")
+        ml_executing = executing
+        didChangeValueForKey("isExecuting")
+    }
+    
+    convenience init(operationBlock:MLAsyncOperationBlock) {
+        self.init()
+        block = operationBlock
+    }
+    
+    override func start() {
+        if cancelled {
+            ml_setFinished(true)
+            return
+        }
+        ml_setExecuting(true)
+        block?(operation: self)
+    }
+    
+    func finishOperation(){
+        ml_setExecuting(false)
+        ml_setFinished(true)
+    }
+    
+    deinit{
+        print("operation deinited")
+    }
+}
+
+extension NSOperationQueue {
+    func addAsyncOperationWithBlock(block:MLAsyncOperationBlock){
+        let asyncOperation = MLAsyncOperation(operationBlock: block)
+        addOperation(asyncOperation)
+    }
+}
--- asyncOperation/main.swift
+++ asyncOperation/main.swift
@@ -8,5 +8,80 @@
 
 import Foundation
 
-print("Hello, World!")
+class UnsafeClass {
+    let queue = dispatch_queue_create("background_queue", DISPATCH_QUEUE_CONCURRENT)
+    func method1(){
+        print("method 1 begin")
+        for _ in 0 ... 100000 {
+            break
+        }
+        print("method 1 end")
+    }
+    
+    func method2(){
+        print("method 2 begin")
+        for _ in 0 ... 100000 {
+            break
+        }
+        print("method 2 end")
+    }
+    
+    func asyncMethod(done:()->Void){
+        print("asyncMethod begin")
+        dispatch_async(queue) { () -> Void in
+            for _ in 0 ... 1000000 {
+                break
+            }
+            print("asyncMethod end")
+            done()
+        }
+        print("asyncMethod dispatched")
+        return
+    }
+}
+
+let operationQueue = NSOperationQueue()
+operationQueue.maxConcurrentOperationCount = 1
+
+autoreleasepool { () -> () in
+
+    
+    let unsafeObject = UnsafeClass()
+//    operationQueue.addAsyncOperationWithBlock{(op) -> Void in
+//        unsafeObject.asyncMethod({ () -> Void in
+//            op?.finishOperation()
+//        })
+//    }
+
+    operationQueue.addOperationWithBlock({ () -> Void in
+        unsafeObject.asyncMethod({ () -> Void in
+            
+        })
+    })
+    operationQueue.addOperationWithBlock({ () -> Void in
+        print("async method begin 2")
+        unsafeObject.asyncMethod({ () -> Void in
+            print("async method end 2")
+        })
+    })
+    operationQueue.addOperationWithBlock { () -> Void in
+        unsafeObject.method2()
+    }
+    
+    operationQueue.addOperationWithBlock { () -> Void in
+        unsafeObject.method1()
+    }
+    
+    let runloop = NSRunLoop.currentRunLoop()
+    while runloop.runMode(NSDefaultRunLoopMode, beforeDate: NSDate.distantFuture()){
+        break
+    }
+
+}
+
+
+
+
+
+
 
