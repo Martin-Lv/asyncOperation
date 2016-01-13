7ba4432cc0aeea94e12f369d73f6562876903ac0
Martin Lv <lvmenglineric@gmail.com>
2016-01-13 22:51:29 +0800

    Initial Commit

---
+++ asyncOperation/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  asyncOperation
+//
+//  Created by 吕孟霖 on 16/1/13.
+//  Copyright © 2016年 lvmenglin. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
