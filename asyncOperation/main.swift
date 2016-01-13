4d8ece9a058d0ba2947a6d1e6f66ab5a8cde8658
Martin Lv <lvmenglineric@gmail.com>
2016-01-14 00:42:20 +0800

    update test code

--- asyncOperation/main.swift
+++ asyncOperation/main.swift
@@ -9,7 +9,7 @@
 import Foundation
 
 class UnsafeClass {
-    let queue = dispatch_queue_create("background_queue", DISPATCH_QUEUE_CONCURRENT)
+    let queue = dispatch_queue_create("UnsafeClass_background_queue", DISPATCH_QUEUE_CONCURRENT)
     func method1(){
         print("method 1 begin")
         for _ in 0 ... 100000 {
@@ -27,12 +27,10 @@ class UnsafeClass {
     }
     
     func asyncMethod(done:()->Void){
-        print("asyncMethod begin")
         dispatch_async(queue) { () -> Void in
             for _ in 0 ... 1000000 {
                 break
             }
-            print("asyncMethod end")
             done()
         }
         print("asyncMethod dispatched")
@@ -41,23 +39,25 @@ class UnsafeClass {
 }
 
 let operationQueue = NSOperationQueue()
+operationQueue.name = "operation_queue"
 operationQueue.maxConcurrentOperationCount = 1
+let backgroundQueue = dispatch_queue_create("background_queue", DISPATCH_QUEUE_CONCURRENT)
 
 autoreleasepool { () -> () in
 
-    
     let unsafeObject = UnsafeClass()
-//    operationQueue.addAsyncOperationWithBlock{(op) -> Void in
-//        unsafeObject.asyncMethod({ () -> Void in
-//            op?.finishOperation()
-//        })
-//    }
 
     operationQueue.addOperationWithBlock({ () -> Void in
+        print("async method begin 1")
         unsafeObject.asyncMethod({ () -> Void in
-            
+            print("async method end 1")
         })
     })
+    operationQueue.addOperationWithBlock { () -> Void in
+        dispatch_async(backgroundQueue, { () -> Void in
+            unsafeObject.method1()
+        })
+    }
     operationQueue.addOperationWithBlock({ () -> Void in
         print("async method begin 2")
         unsafeObject.asyncMethod({ () -> Void in
@@ -65,12 +65,12 @@ autoreleasepool { () -> () in
         })
     })
     operationQueue.addOperationWithBlock { () -> Void in
-        unsafeObject.method2()
+        dispatch_async(backgroundQueue, { () -> Void in
+            unsafeObject.method2()
+        })
     }
     
-    operationQueue.addOperationWithBlock { () -> Void in
-        unsafeObject.method1()
-    }
+
     
     let runloop = NSRunLoop.currentRunLoop()
     while runloop.runMode(NSDefaultRunLoopMode, beforeDate: NSDate.distantFuture()){
